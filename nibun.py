d505f6f05c915d7c964aac8443c2c2fdcc9dc745
zzzz46 <104346940+zzzz46@users.noreply.github.com>
2022-08-09 18:33:10 +0900

    Update nibun.py

--- nibun.py
+++ nibun.py
@@ -62,6 +62,16 @@ def visualization(func_f, x_min, x_max, x_solved):
 
     plt.scatter(x_solved, 0.0)  #数値解を点グラフで表示
     plt.text(x_solved, 0.5, 'y=x^2.0 - 1.0')
+    plt.text(
+        a,
+        0.0,
+        "a",
+    )
+    plt.text(
+        b,
+        0.0,
+        "b",
+    )
     plt.text(x_solved,
              0.0,
              "$x$ = {:.9f}".format(x_solved),
